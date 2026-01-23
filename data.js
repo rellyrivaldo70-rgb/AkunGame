1efdf1f5dd039a0f1a3d7ab4f231b930901b77a9
rellyrivaldo70-rgb <rellyrivaldo70@gmail.com>
2026-01-23 09:02:30 +0700

    Add akun data and contact information

---
+++ data.js
@@ -0,0 +1,21 @@
+const akun = [
+  {
+    id: 1,
+    judul: "Akun Mobile Legends Mythic",
+    harga: "Rp250.000",
+    status: "available",
+    gambar: "img/ml.jpg",
+    deskripsi: "Mythic 120⭐, full hero, email aman"
+  },
+  {
+    id: 2,
+    judul: "Akun Free Fire Sultan",
+    harga: "Rp400.000",
+    status: "sold",
+    gambar: "img/ff.jpg",
+    deskripsi: "Bundle langka, senjata full evo"
+  }
+];
+
+const whatsapp = "628123456789";
+const telegram = "usernamekamu";
